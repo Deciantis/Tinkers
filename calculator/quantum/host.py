7f7550f57b36b5e177103faf6e2827219fe1e622
Deneuve B <7785171+Deciantis@users.noreply.github.com>
2022-02-27 12:46:14 -0500

    ready to implement quantum calculations

--- calculator/quantum/host.py
+++ calculator/quantum/host.py
@@ -1,5 +1,5 @@
 import qsharp 
-from HostPython import SayHello
+from HostPython import Calculate
 
 print("\n\nWelcome to the quantum calculator!\n")
 print("Please enter two numbers you would like to Add\n")
@@ -7,5 +7,5 @@ print("Please enter two numbers you would like to Add\n")
 s = input()
 numbers = list(map(int, s.split()))
 
-print("These are the numbers you enetered " + str(numbers))
-print(SayHello.simulate(numbers = numbers))
+print("\nYou enetered " + str(numbers))
+print(Calculate .simulate(numbers = numbers))
